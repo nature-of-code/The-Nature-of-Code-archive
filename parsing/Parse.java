ff5499dbb3a04278fa13694353d33146133ea66f
Daniel Shiffman <daniel@shiffman.net>
2012-10-14 15:17:49 -0400

    working on parser

--- parsing/Parse.java
+++ parsing/Parse.java
@@ -8,55 +8,78 @@ class Parse {
 
 
 		String path = "/Users/shiffman/Documents/The-Nature-of-Code/raw/chapters/";
-		String filename = args[0];
 
-		String fullpath = path + filename;
 
-		// Reading the file
-		BufferedReader br = new BufferedReader(new FileReader(fullpath));
- 		String line;
+		String[] filenames;
+		if (args.length < 1) {
+ 			filenames = listFileNames(path);
+		} else {
+			filenames = args;
+		}
 
- 		// Clearning the output file
-    	BufferedWriter clear = new BufferedWriter(new FileWriter(filename));
-    	clear.close();
+		for (int i = 0; i < filenames; i++) {
+			String fullpath = path + filename;
 
-    	// Getting ready to append
-    	BufferedWriter out = new BufferedWriter(new FileWriter(filename, true));
+			// Reading the file
+			BufferedReader br = new BufferedReader(new FileReader(fullpath));
+	 		String line;
+
+ 			// Clearning the output file
+    		BufferedWriter clear = new BufferedWriter(new FileWriter(filename));
+    		clear.close();
+
+    		// Getting ready to append
+    		BufferedWriter out = new BufferedWriter(new FileWriter(filename, true));
 		
-    	boolean source = false;
-    	boolean ignore = false;
+	    	boolean source = false;
+    		boolean ignore = false;
 
 
-    	int counter = 0;
+    		int counter = 0;
 
-		while ((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 
-			if (line.contains("[source,java]")) {
-				source = true;
-				counter = 0;
-				//System.out.println("SOURCE ON: " + line);
-			} else if (line.contains("----") && source) {
-				counter++;
-				//System.out.println("Counting: " + counter);
-				if (counter == 2) {
-				  source = false;
-				  //System.out.println("SOURCE OFF: " + line);
+				if (line.contains("[source,java]")) {
+					source = true;
+					counter = 0;
+					//System.out.println("SOURCE ON: " + line);
+				} else if (line.contains("----") && source) {
+					counter++;
+					//System.out.println("Counting: " + counter);
+					if (counter == 2) {
+					  source = false;
+					  //System.out.println("SOURCE OFF: " + line);
+					}
+				} 
+
+				if (line.trim().equals("++++")) {
+					ignore = !ignore;
 				}
-			} 
-
-			if (line.trim().equals("++++")) {
-				ignore = !ignore;
-			}
 			
-			if (line.contains("PVector") && !source && !ignore) {
-				line = line.replaceAll("([^*])PVector([^*])","$1[klass]*PVector*$2");
-				System.out.println("-------------------------------------");
-				System.out.println(line);
-				System.out.println("-------------------------------------");
+				if (line.contains("PVector") && !source && !ignore) {
+					line = line.replaceAll("([^*])PVector([^*])","$1[klass]*PVector*$2");
+					System.out.println("-------------------------------------");
+					System.out.println(line);
+					System.out.println("-------------------------------------");
+				}
+				out.write(line + "\n");
+
 			}
-			out.write(line + "\n");
+			out.close();
 
 		}
-		out.close();
+
 	}
+
+	// This function returns all the files in a directory as an array of Strings  
+	public static String[] listFileNames(String dir) {
+  		File file = new File(dir);
+  		if (file.isDirectory()) {
+    		String names[] = file.list();
+    		return names;
+  	} else {
+    	// If it's not a directory
+    	return null;
+  	}
+}
 }
