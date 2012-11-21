e943c835f37c920709e8700f8068dff4bb54a77a
Daniel Shiffman <daniel@shiffman.net>
2012-11-21 14:10:13 -0500

    working on math notation chapter 3

--- parsing/NOCParsing/src/ParseMath.java
+++ parsing/NOCParsing/src/ParseMath.java
@@ -20,7 +20,7 @@ class ParseMath {
 				String mathml = m.group(2);
 
 				//System.out.println(name);
-				if (name.equals("Cd")) {
+				if (name.equals("arctangentvelocity")) {
 
 					System.out.println("Writing: " + name + ".mathml");
 					//System.out.println(name);
